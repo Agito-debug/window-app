05e20c57a768b7b06ad4b85b7991facedba384d2
Denislav <evita10@abv.bg>
2021-03-29 11:31:12 +0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-green-500 h-screen">
+    <div class="container mx-auto p-2">
+      <div
+        class="my-20 rounded-lg shadow-xl max-w-sm mx-auto bg-white px-5 py-10"
+      >
+        <h3 class="mb-8 text-center text-green-700">Mixvoip</h3>
+        <form>
+          <div class="w-full">
+            <video ref="videoRef" autoplay="true" />
+          </div>
+          <div>
+            <label ref="">Video source: </label>
+            <select v-model="videoSelect"></select>
+          </div>
+          <div>
+            <label ref="">Audio source: </label>
+            <select v-model="videoSelect"></select>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const videoRef = ref("");
+    const audioSelect = ref("");
+    const videoSelect = ref("");
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        videoRef.value.srcObject = stream;
+      });
+    }
+
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(function (devices) {
+        devices.forEach(function (device) {
+          console.log(
+            device.kind + ": " + device.label + " id = " + device.deviceId
+          );
+        });
+      })
+      .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+      });
+
+    return { videoRef, audioSelect, videoSelect };
+  },
+};
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
